1716a34b643e5e8d343f6ad0822f6828c9acb8eb
iamrohitt <rohitpaswan85@gmail.com>
2023-07-06 01:25:16 +0545

    hover anim

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,38 +12,66 @@ const Navbar = () => {
   };
 
   return (
-    <MDBNavbar expand='lg' light bgColor='light' fixed='top'>
-      <MDBContainer fluid>
-        <Link to="/" className='navbar-brand'>
-          Navbar
-        </Link>
-        <MDBNavbarToggler onClick={toggleCollapse}>
-          <Icon path={isOpen ? mdiClose : mdiMenu} size={1} />
-        </MDBNavbarToggler>
-        <MDBCollapse show={isOpen} navbar>
-          <MDBNavbarNav className='mr-auto' style={{ marginLeft: '800px' }}>
-            {/* About */}
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#'>About</MDBNavbarLink>
-            </MDBNavbarItem>
-            {/* Services */}
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Services</MDBNavbarLink>
-            </MDBNavbarItem>
-            {/* Contact */}
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Contact</MDBNavbarLink>
-            </MDBNavbarItem>
-          </MDBNavbarNav>
-          {/* Login/Register */}
-          <MDBNavbarNav className='ml-auto' style={{ marginLeft: '100px' }}>
-            <MDBNavbarItem>
-              <Link to="/login" className='nav-link'>Login/Signup</Link>
-            </MDBNavbarItem>
-          </MDBNavbarNav>
-        </MDBCollapse>
-      </MDBContainer>
-    </MDBNavbar>
+    <>
+      <style>
+        {`
+          .hover-underline-animation {
+            position: relative;
+            color: #0087ca;
+          }
+
+          .hover-underline-animation:after {
+            content: '';
+            position: absolute;
+            width: 100%;
+            transform: scaleX(0);
+            height: 2px;
+            bottom: 0;
+            left: 0;
+            background-color: #0087ca;
+            transform-origin: bottom right;
+            transition: transform 0.25s ease-out;
+          }
+
+          .hover-underline-animation:hover:after {
+            transform: scaleX(1);
+            transform-origin: bottom left;
+          }
+        `}
+      </style>
+      <MDBNavbar expand='lg' light bgColor='light' fixed='top'>
+        <MDBContainer fluid>
+          <Link to="/" className='navbar-brand'>
+            Navbar
+          </Link>
+          <MDBNavbarToggler onClick={toggleCollapse}>
+            <Icon path={isOpen ? mdiClose : mdiMenu} size={1} />
+          </MDBNavbarToggler>
+          <MDBCollapse show={isOpen} navbar>
+            <MDBNavbarNav className='mr-auto 'style={{marginLeft:'800px'}}>
+              {/* About */}
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#' className='hover-underline-animation'>About</MDBNavbarLink>
+              </MDBNavbarItem>
+              {/* Services */}
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#' className='hover-underline-animation'>Services</MDBNavbarLink>
+              </MDBNavbarItem>
+              {/* Contact */}
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#' className='hover-underline-animation'>Contact</MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+            {/* Login/Register */}
+            <MDBNavbarNav className='ml-auto'style={{marginLeft:'100px'}}>
+              <MDBNavbarItem>
+                <Link to="/login" className='nav-link hover-underline-animation'>Login/Signup</Link>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </>
   );
 };
 
