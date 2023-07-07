b97aca273432d4ce898922886e21cd32b33aa239
susanhsrestha <susanshrestha783@gmail.com>
2023-07-07 23:16:09 +0545

    minor bug fixes

--- frontend/src/pages/Predict.jsx
+++ frontend/src/pages/Predict.jsx
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Predict = () => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, [token, navigate]);
   const [inputValue, setInputValue] = useState("");
   const [predictionResult, setPredictionResult] = useState(null);
 
