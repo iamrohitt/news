64b7b040773613c3f55e732cfb65f24e2626049b
susanhsrestha <susanshrestha783@gmail.com>
2023-07-09 21:10:17 +0545

    updated news fetching mechanism

--- backend/Controllers/NewsController.js
+++ backend/Controllers/NewsController.js
@@ -1,12 +1,17 @@
 const Related = require("../Models/NewsModel"); // Import the Related model
 
 exports.getAllRelatedNews = async (req, res) => {
+    const { start, end } = req.query;
+
     Related.find({})
-        .then(data => {
+        .sort({ timestamp: -1 }) // Sort documents in descending order based on the timestamp
+        .skip(parseInt(start))
+        .limit(parseInt(end))
+        .then((data) => {
             res.json(data);
         })
-        .catch(error => {
-            console.error('Error fetching data from MongoDB:', error);
-            res.status(500).json({ error: 'Failed to fetch data from MongoDB' });
+        .catch((error) => {
+            console.error("Error fetching data from MongoDB:", error);
+            res.status(500).json({ error: "Failed to fetch data from MongoDB" });
         });
 };
--- frontend/src/components/CardBox.jsx
+++ frontend/src/components/CardBox.jsx
@@ -13,21 +13,21 @@ import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 import "../css/CardBox.css";
 
-// Styled component for customizing the Card
 const StyledCard = styled(Card)({
   marginBottom: 10,
   backgroundColor: "rgba(255, 255, 255, 0.7)",
   backdropFilter: "blur(5px)",
   boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
   borderRadius: "10px",
-  width: "375px", // Set a constant width for the card
+  width: "375px",
 });
-// Styled component for customizing the CardContent
+
 const CustomCardContent = styled(CardContent)({
-  height: "200px", // Set a fixed height for the card content
-  overflow: "hidden", // Hide any overflow content
+  height: "200px",
+  overflow: "hidden",
 });
 
 const CustomCardContent2 = styled(CardContent)({
@@ -39,116 +39,147 @@ const CustomCardContent2 = styled(CardContent)({
 });
 
 const CardBox = () => {
-  // State variables
   const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [showReadMore, setShowReadMore] = useState(true);
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(30);
   const token = localStorage.getItem("token");
-  console.log(token);
-  // State variables
-  useEffect(() => {
-    // Fetch news data when component mounts
-    fetchNews();
-  }, []);
 
-  const fetchNews = () => {
-    // Get the token from localStorage
-    const token = localStorage.getItem("token");
-
-    // Create the Axios request config with the token in the headers
+  const fetchNews = async () => {
     const config = {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     };
 
-    // Fetch news from the server using Axios with the token included in the headers
-    axios
-      .get("http://localhost:4000/api/news/related", config)
-      .then((response) => {
-        setNews(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching news:", error);
-      });
+    setLoading(true);
+
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/api/news/related?start=${start}&end=${end}`,
+        config
+      );
+
+      if (response.data.length > 0) {
+        setNews((prevNews) => [...prevNews, ...response.data]);
+        setStart((prevStart) => prevStart + 50);
+        setEnd((prevEnd) => prevEnd + 50);
+      } else {
+        setShowReadMore(false);
+      }
+    } catch (error) {
+      console.error("Error fetching news:", error);
+    } finally {
+      setLoading(false);
+    }
   };
 
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
-      overflow="auto"
-      paddingTop="100px" // Add padding top to accommodate the navbar height
-      style={{
-        background: "linear-gradient(to bottom right, #74ebd5, #9face6)",
-      }}
-    >
-      <div className="card-container">
-        {news.length > 0 ? (
-          // Render each news item as a Card
-          news.map((item) => (
-            <div className="card-item" key={item.id}>
-              <StyledCard>
-                <CardHeader
-                  title={<TfiTwitter size="30px" color="black" />}
-                  action={
-                    <Box display="flex" alignItems="center">
-                      <BsClock
-                        size={15}
-                        color="textSecondary"
-                        style={{ marginRight: "6px", marginTop: "7px" }}
-                      />
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        sx={{ marginTop: "12px" }}
-                      >
-                        {moment(item.timestamp).startOf("hour").fromNow()}
-                      </Typography>
-                    </Box>
-                  }
-                />
-                <CustomCardContent>
-                  <Typography>{item.text}</Typography>
-                </CustomCardContent>
-                <CustomCardContent2>
-                  {item.class && <Typography>{item.class}</Typography>}
-                </CustomCardContent2>
-                <CardActions disableSpacing>
-                  <Button
-                    variant="contained"
-                    href={item.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    sx={{
-                      marginTop: "5px",
-                      marginBottom: "5px",
-                      backgroundColor: "#FF4081",
-                      color: "white",
-                      "&:hover": {
-                        background:
-                          "linear-gradient(to bottom right, #74ebd5, #9face6)",
-                      },
-                    }}
-                  >
-                    READ MORE
-                  </Button>
-                  <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                  </IconButton>
-                  <IconButton aria-label="share">
-                    <ShareIcon />
-                  </IconButton>
-                </CardActions>
-              </StyledCard>
+    <div className="bgbg">
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+        overflow="auto"
+        paddingTop="100px"
+        style={{
+          background: "linear-gradient(to bottom right, #74ebd5, #9face6)",
+        }}
+      >
+        <div className="card-container">
+          {loading ? (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <CircularProgress />
             </div>
-          ))
-        ) : (
-          // Display a message if no news is available
-          <Typography>No news available</Typography>
-        )}
-      </div>
-    </Box>
+          ) : news.length > 0 ? (
+            news.map((item) => (
+              <div className="card-item" key={item.id}>
+                <StyledCard>
+                  <CardHeader
+                    title={<TfiTwitter size="30px" color="black" />}
+                    action={
+                      <Box display="flex" alignItems="center">
+                        <BsClock
+                          size={15}
+                          color="textSecondary"
+                          style={{ marginRight: "6px", marginTop: "7px" }}
+                        />
+                        <Typography
+                          variant="body2"
+                          color="textSecondary"
+                          sx={{ marginTop: "12px" }}
+                        >
+                          {moment(item.timestamp).startOf("hour").fromNow()}
+                        </Typography>
+                      </Box>
+                    }
+                  />
+                  <CustomCardContent>
+                    <Typography align="justify">{item.text}</Typography>
+                  </CustomCardContent>
+                  <CustomCardContent2>
+                    {item.class && <Typography>{item.class}</Typography>}
+                  </CustomCardContent2>
+                  <CardActions disableSpacing>
+                    <Button
+                      variant="contained"
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      sx={{
+                        marginTop: "5px",
+                        marginBottom: "5px",
+                        backgroundColor: "#FF4081",
+                        color: "white",
+                        "&:hover": {
+                          background:
+                            "linear-gradient(to bottom right, #74ebd5, #9face6)",
+                        },
+                      }}
+                    >
+                      READ MORE
+                    </Button>
+                    <IconButton aria-label="add to favorites">
+                      <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="share">
+                      <ShareIcon />
+                    </IconButton>
+                  </CardActions>
+                </StyledCard>
+              </div>
+            ))
+          ) : (
+            <Typography>No news available</Typography>
+          )}
+        </div>
+      </Box>
+      {showReadMore && (
+        <div className="more-news-container">
+          <Button
+            variant="contained"
+            onClick={fetchNews}
+            disabled={loading}
+            sx={{
+              backgroundColor: "#FF4081",
+              color: "white",
+              "&:hover": {
+                background:
+                  "linear-gradient(to bottom right, #74ebd5, #9face6)",
+              },
+            }}
+          >
+            {loading ? "Loading..." : "More News"}
+          </Button>
+        </div>
+      )}
+    </div>
   );
 };
 
