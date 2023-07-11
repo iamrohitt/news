4a40b61d7949f7807c393c05b2d621df0427b1c5
susanhsrestha <susanshrestha783@gmail.com>
2023-07-11 23:21:10 +0545

    updated navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,8 +15,8 @@ import newspaper from "./newspaper.png"; // Import the PNG image
 import "../css/Navbar.css";
 
 const Navbar = () => {
-  const navigate = useNavigate(); // Changed variable name to lowercase
-  const location = useLocation(); // Get the current location
+  const navigate = useNavigate();
+  const location = useLocation();
   const token = localStorage.getItem("token");
   const [isOpen, setIsOpen] = useState(false);
   const [isNavbarVisible, setIsNavbarVisible] = useState(true);
@@ -26,9 +26,7 @@ const Navbar = () => {
   };
 
   const handleLogout = () => {
-    // Remove the token from localStorage
     localStorage.removeItem("token");
-    // Redirect to the login page
     navigate("/login");
   };
 
@@ -48,85 +46,89 @@ const Navbar = () => {
   }, []);
 
   return (
-    <>
-      {isNavbarVisible && (
-        <MDBNavbar expand="lg" light bgColor="light" fixed="top">
-          <MDBContainer fluid>
-            <Link to="/" className="navbar-brand">
-              <img src={newspaper} alt="newspaper" />
-              <span className="navbar-heading">DNC</span>
-            </Link>
-            <div className="nav-style">
-              <MDBNavbarToggler onClick={toggleCollapse}>
-                <Icon path={isOpen ? mdiClose : mdiMenu} size={1} />
-              </MDBNavbarToggler>
-              <MDBCollapse show={isOpen} navbar>
-                <MDBNavbarNav className="ml-auto">
-                  {token && (
-                    <MDBNavbarItem>
-                      <Link
-                        to="/"
-                        className={`nav-link hover-underline-animation ${
-                          location.pathname === "/" ? "active" : ""
-                        }`}
-                      >
-                        Home
-                      </Link>
-                    </MDBNavbarItem>
-                  )}
-                  {/* About */}
+    <div
+      className={`navbar-wrapper ${isNavbarVisible ? "visible" : "hidden"} nav`}
+    >
+      <MDBNavbar
+        expand="lg"
+        bgColor="background-color: linear-gradient(to bottom right, #74ebd5, #9face6);"
+        fixed="top"
+      >
+        <MDBContainer fluid>
+          <Link to="/" className="navbar-brand">
+            <img src={newspaper} alt="newspaper" />
+            <span className="navbar-heading">DNC</span>
+          </Link>
+          <div className="nav-style">
+            <MDBNavbarToggler onClick={toggleCollapse}>
+              <Icon path={isOpen ? mdiClose : mdiMenu} size={1} />
+            </MDBNavbarToggler>
+            <MDBCollapse show={isOpen} navbar>
+              <MDBNavbarNav className="ml-auto">
+                {token && (
                   <MDBNavbarItem>
-                    <MDBNavbarLink
-                      href="#"
-                      className={`hover-underline-animation ${
-                        location.pathname === "/about" ? "active" : ""
+                    <Link
+                      to="/"
+                      className={`nav-link hover-underline-animation ${
+                        location.pathname === "/" ? "active" : ""
                       }`}
                     >
-                      About
-                    </MDBNavbarLink>
+                      Home
+                    </Link>
                   </MDBNavbarItem>
-                  {/* Predict */}
-                  {token && (
-                    <MDBNavbarItem>
-                      <Link
-                        to="/predict"
-                        className={`nav-link hover-underline-animation ${
-                          location.pathname === "/predict" ? "active" : ""
-                        }`}
-                      >
-                        Predict
-                      </Link>
-                    </MDBNavbarItem>
-                  )}
-                </MDBNavbarNav>
-                {/* Login/Register or Logout */}
-                <MDBNavbarNav className="ml-auto">
+                )}
+                {/* About */}
+                <MDBNavbarItem>
+                  <MDBNavbarLink
+                    href="#"
+                    className={`hover-underline-animation ${
+                      location.pathname === "/about" ? "active" : ""
+                    }`}
+                  >
+                    About
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+                {/* Predict */}
+                {token && (
                   <MDBNavbarItem>
-                    {token ? (
-                      <button
-                        className="nav-link hover-underline-animation"
-                        onClick={handleLogout}
-                      >
-                        Logout
-                      </button>
-                    ) : (
-                      <Link
-                        to="/login"
-                        className={`nav-link hover-underline-animation ${
-                          location.pathname === "/login" ? "active" : ""
-                        }`}
-                      >
-                        Login/Signup
-                      </Link>
-                    )}
+                    <Link
+                      to="/predict"
+                      className={`nav-link hover-underline-animation ${
+                        location.pathname === "/predict" ? "active" : ""
+                      }`}
+                    >
+                      Predict
+                    </Link>
                   </MDBNavbarItem>
-                </MDBNavbarNav>
-              </MDBCollapse>
-            </div>
-          </MDBContainer>
-        </MDBNavbar>
-      )}
-    </>
+                )}
+              </MDBNavbarNav>
+              {/* Login/Register or Logout */}
+              <MDBNavbarNav className="ml-auto">
+                <MDBNavbarItem>
+                  {token ? (
+                    <button
+                      className="nav-link hover-underline-animation"
+                      onClick={handleLogout}
+                    >
+                      Logout
+                    </button>
+                  ) : (
+                    <Link
+                      to="/login"
+                      className={`nav-link hover-underline-animation ${
+                        location.pathname === "/login" ? "active" : ""
+                      }`}
+                    >
+                      Login/Signup
+                    </Link>
+                  )}
+                </MDBNavbarItem>
+              </MDBNavbarNav>
+            </MDBCollapse>
+          </div>
+        </MDBContainer>
+      </MDBNavbar>
+    </div>
   );
 };
 
